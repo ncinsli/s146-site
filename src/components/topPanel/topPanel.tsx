4118c85314e24066f9074ad113ee370d83ae1769
Василий Гайсен <56553344+ncinsli@users.noreply.github.com>
2020-11-15 16:32:33 +0500

    No parallax now

--- src/components/topPanel/topPanel.tsx
+++ src/components/topPanel/topPanel.tsx
@@ -2,17 +2,14 @@ import React from "react";
 import "./topPanelStyle.scss";
 import bits from "./bits.png";
 import atom from "./atom.png"
-import { Parallax, Background } from 'react-parallax';
 
 class TopPanel extends React.Component{
     render(){
         return(
             <React.Fragment>
                 <div className = "topPanel">
-                    <Parallax strength = {200}>
-                        <img className = "randomDecorations" src = {atom}/>
-                        <img className = "randomDecorations" src = {bits}/>
-                    </Parallax>
+                    <img className = "randomScalingDecorations" src = {atom}/>
+                    <img className = "randomDecorations" src = {bits}/>
                     <p className = "hidden">заполнитель на случай, если уйдут все картинки</p> { /* заполнитель */ }
                 </div>
             </React.Fragment>

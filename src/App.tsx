67d3b41e3fc05e2d0e8d1306f3a3e3a7230e7115
Vitasha <vitaliy.suhoplechev.1@gmail.com>
2021-10-03 18:48:55 +0500

    Cleaning
    Cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import "./App.scss";
 import React from "react";
-import Button from "./components/Buttons/Button";
-import TopButton from "./components/Buttons/TopButton";
-import {Carousel} from "react-responsive-carousel";
-import {observable} from "mobx";
-import SideButton from "./components/Buttons/SideButton";
-import Logo from "./components/Logo/Logo";
 import UpperDecoration from "./components/UpperDecoration/UpperDecoration";
-import ArticleCard from "./components/Articles/ArticleCard";
 import TopMenu from "./components/Complicated/TopMenu/TopMenu";
 
 const App : React.FC = () => {
@@ -15,7 +8,7 @@ const App : React.FC = () => {
         <React.Fragment>
             <UpperDecoration/>
 
-            <TopMenu className="TopMenubar"/>
+            <TopMenu className="TopMenu"/>
 
        </React.Fragment>
     );
